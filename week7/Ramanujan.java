b2075e323aecd6da06a767413a35fa2e634e2b69
OlegSmac <oleansmacinih@gmail.com>
2021-04-21 23:03:04 +0300

    week7

--- week7/Ramanujan.java
+++ week7/Ramanujan.java
@@ -1,13 +1,17 @@
 public class Ramanujan {
     public static boolean isRamanujan(long n) {
         int k = 0;
-        long c = Math.round(Math.pow(n, 1.0/3));
-        for (int i = 1; i <= c; i++) {
-            long j = Math.round(Math.pow(n - Math.pow(i, 3), 1.0/3));
-            if (Math.pow(i, 3) + Math.pow(j, 3) == n && i < j) {
+        for (long i = 1; i < Math.cbrt(n/2.0); i++) {
+            long i3 = i * i * i;
+            long j = (long) Math.cbrt(n - i3);
+            long j3 = j * j * j;
+
+            if (i3 + j3 == n) {
                 k += 1;
             }
-            if (k == 2) break;
+            if (k == 2) {
+                break;
+            }
         }
         return k == 2;
     }
